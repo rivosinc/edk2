4887d105411120882d9bf7213c171d204e64bd34
Dhaval <dhaval@rivosinc.com>
2024-08-12 18:08:46 +0530

    UefiPayloadPkg: Enable RiscV64 entry point to UPL
    As per specification we are going to accept only one argument
    at the entry point which is FDT pointer. Grab that and call
    the entry point.
    
    Signed-off-by: Dhaval Sharma <dhaval@rivosinc.com>

---
+++ UefiPayloadPkg/UefiPayloadEntry/RiscV64/DxeLoadFuncFit.c
@@ -0,0 +1,33 @@
+/** @file
+  x64-specifc functionality for DxeLoad.
+
+Copyright (c) 2006 - 2020, Intel Corporation. All rights reserved.<BR>
+SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#include <PiPei.h>
+#include <Library/BaseLib.h>
+#include <Library/DebugLib.h>
+#include <Library/BaseMemoryLib.h>
+#include <Library/MemoryAllocationLib.h>
+#include <Library/PcdLib.h>
+#include <Library/HobLib.h>
+#include <Library/FdtLib.h>
+#include <Library/PcdLib.h>
+#include "UefiPayloadEntry.h"
+
+/**
+  Entry point to the C language phase of UEFI payload.
+  @param[in]   BootloaderParameter    The starting address of bootloader parameter block.
+  @retval      It will not return if SUCCESS, and return error when passing bootloader parameter.
+**/
+EFI_STATUS
+EFIAPI
+_ModuleEntryPoint (
+  IN UINTN  Param1,
+  IN UINTN  Param2
+  )
+{
+  return FitUplEntryPoint (Param2);
+}
