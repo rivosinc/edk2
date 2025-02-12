5fb82bb2c9c36abdef1dc802a34a744601762e09
Dhaval <dhaval@rivosinc.com>
2025-02-12 20:28:32 +0530

    StandaloneMmPkg: Implement communication with secure monitor based on RPMI spec
    Implementation is based on https://github.com/riscv-non-isa/riscv-rpmi/tree/main and works in the following way:
    1. MM initialization is successful -> call REQFWD_COMPLETE_CURRENT_MESSAGE to yield. (this API will change once
        we have properly defined the init API for MM)
    2. NS domain calls MM_COMMUNICATE, OpenSBI performs context switch (if channel is not capable of SSE)
        -> MM resumes loop from REQFWD_COMPLETE_CURRENT_MESSAGE.
    3. Calls REQFWD_RETRIEVE_CURRENT_MESSAGE.
    4. MM handles the request contained in the message.
    5. MM calls REQFWD_COMPLETE_CURRENT_MESSAGE.
    6. OpenSBI switches to the non-secure domain (as there are no more messages in the queue && SSE is not supported)
    7.  Go to #2

--- MdePkg/Library/StandaloneMmCoreEntryPoint/RiscV64/StandaloneMmCoreEntryPoint.h
+++ MdePkg/Library/StandaloneMmCoreEntryPoint/RiscV64/StandaloneMmCoreEntryPoint.h
@@ -13,6 +13,7 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 #include <Uefi.h>
 #include <Library/PeCoffLib.h>
 #include <Library/FvLib.h>
+#include "../../../OvmfPkg/RiscVVirt/MmCommunicationDxe/MmCommunicate.h"
 
 typedef struct {
   UINT32    ProcessorId;
@@ -43,6 +44,62 @@ typedef struct {
 #define BOOT_INFO_STACK_BASE_OFFSET  24   // Used in assembly
 #define CPU_INFO_FLAG_PRIMARY_CPU    0x00000001
 STATIC_ASSERT (BOOT_INFO_STACK_BASE_OFFSET == OFFSET_OF (EFI_RISCV_SMM_PAYLOAD_INFO, MmStackBase));
+
+// ReqFwd Services
+typedef enum {
+  REQFWD_ENABLE_NOTIFICATION      = 0x1,
+  REQFWD_RETRIEVE_CURRENT_MESSAGE = 0x2,
+  REQFWD_COMPLETE_CURRENT_MESSAGE = 0x3
+} ReqFwdService;
+
+// SMM return error codes
+#define RISCV_SMM_RET_SUCCESS         0
+#define RISCV_SMM_RET_NOT_SUPPORTED   -1
+#define RISCV_SMM_RET_INVALID_PARAMS  -2
+#define RISCV_SMM_RET_DENIED          -3
+#define RISCV_SMM_RET_NO_MEMORY       -4
+
+#pragma pack(1) // Ensure no padding between fields
+// RPMI message header structure
+typedef struct {
+  UINT16    ServiceGroupId;      // SERVICEGROUP_ID (16 bits, bits 15:0)
+  UINT8     ServiceId;           // SERVICE_ID (8 bits, bits 23:16)
+  UINT8     Flags;               // FLAGS (8 bits, bits 31:24)
+  UINT16    Token;               // TOKEN (16 bits, bits 31:16 of the second word): Not used in MM
+  UINT16    DataLen;             // DATALEN (16 bits, bits 15:0 of the second word, length of data in bytes, 4-byte multiple)
+} RPMI_MESSAGE_HEADER;
+
+typedef struct {
+  RPMI_MESSAGE_HEADER    hdr;
+  UINT32                 StartIndex;
+} REQFWD_RETRIEVE_CMD;
+
+typedef struct {
+  UINT32    Offset;
+} MM_PAYLOAD;
+
+typedef struct {
+  UINT32    Status;
+  UINT32    Remaining;
+  UINT32    Returned;
+} REQFWD_RETRIEVE_RESP;
+
+typedef struct {
+  RPMI_MESSAGE_HEADER     hdr;
+  REQFWD_RETRIEVE_RESP    reqfwd_resp;
+} RPMI_REQFWD_RETRIEVE_RESP;
+
+typedef struct {
+  RPMI_MESSAGE_HEADER        hdr;
+  RISCV_SMM_MSG_COMM_ARGS    mm_data;
+} RPMI_SMM_MSG_CMPL_CMD;
+
+typedef struct {
+  RPMI_REQFWD_RETRIEVE_RESP    rpmi_resp;
+  RISCV_SMM_MSG_COMM_ARGS      mm_data;
+} RPMI_SMM_MSG_COMM_ARGS;
+
+#pragma pack() // End of packed structure
 
 VOID *
 EFIAPI
