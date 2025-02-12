8b93822fa18af5c44c1c06a74807a6268b475821
Dhaval <dhaval@rivosinc.com>
2025-02-12 20:11:47 +0530

    StandaloneMmPkg/RiscV64: Add boot information hob
    EFI_RISCV_SMM_PAYLOAD_INFO describes the current information passing structure from secure
    monitor to MM.
    TODO: Later at some point of time we would like to move to different interface like DT or equivalent.
    Signed-off-by: Dhaval Sharma <dhaval@rivosinc.com>
    Signed-off-by: Yong Li <yong.li@intel.com>
    Signed-off-by: Tuan Phan <tphan@ventanamicro.com>

---
+++ MdePkg/Library/StandaloneMmCoreEntryPoint/RiscV64/CreateHobList.c
@@ -0,0 +1,255 @@
+/** @file
+  Creates HOB during Standalone MM Foundation entry point
+  on RISCV platforms.
+
+Copyright (c) 2023, Ventana Micro System Inc. All rights reserved.<BR>
+Copyright (c) 2023, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2025, Rivos Inc. All rights reserved.<BR>
+
+SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#include <PiMm.h>
+
+#include <PiPei.h>
+#include <Guid/MmramMemoryReserve.h>
+#include <Guid/MpInformation.h>
+
+#include <StandaloneMmCpu.h>
+#include <StandaloneMmCoreEntryPoint.h>
+#include <Library/DebugLib.h>
+#include <Library/HobLib.h>
+#include <Library/BaseLib.h>
+#include <Library/BaseMemoryLib.h>
+#include <Library/SerialPortLib.h>
+#include <Guid/MmCommBuffer.h>
+
+extern EFI_HOB_HANDOFF_INFO_TABLE *
+HobConstructor (
+  IN VOID   *EfiMemoryBegin,
+  IN UINTN  EfiMemoryLength,
+  IN VOID   *EfiFreeMemoryBottom,
+  IN VOID   *EfiFreeMemoryTop
+  );
+
+// GUID to identify HOB where the entry point of the CPU driver will be
+// populated to allow this entry point driver to invoke it upon receipt of an
+// event
+extern EFI_GUID  gEfiMmCpuDriverEpDescriptorGuid;
+
+/**
+  Build communication buffer HOB.
+
+  @return  EFI_STATUS
+
+      +------------------------+
+      |     Status             |
+      |                        <----+
+      +------------------------+    |
+      |                        +----+
+Page 0|    CommBuffer Struct   |
+      +------------------------+
+      |                        |
+      |                        |
+      |    NS Comm Buffer      |
+Page 1|                        |
+      +------------------------+
+
+  Steal one page from allocated MMCommBuffer and use it
+  for MM_COMM_BUFFER
+*/
+
+/**
+  Build a communication buffer HOB for use by the MM environment.
+
+  This function creates a GUIDed HOB that specifies the location and size of the
+  communication buffer shared between the MM environment and the Normal world.
+  Additionally, it initializes a buffer status structure to indicate readiness
+  for communication.
+
+  @param[in]  PayloadBootInfo  Pointer to the EFI_RISCV_SMM_PAYLOAD_INFO structure
+                               containing the base address and size of the non-secure
+                               communication buffer.
+
+  @retval EFI_SUCCESS           The HOB was created successfully.
+  @retval EFI_INVALID_PARAMETER The base address or size of the communication buffer
+                                 in PayloadBootInfo is zero.
+  @retval EFI_OUT_OF_RESOURCES  Failed to allocate memory for the HOB or buffer status.
+**/
+STATIC
+EFI_STATUS
+MmBuildCommBufferHob (
+  IN       EFI_RISCV_SMM_PAYLOAD_INFO  *PayloadBootInfo
+  )
+{
+  MM_COMM_BUFFER  *MmCommBuffer;
+
+  if ((PayloadBootInfo->MmNsCommBufBase == 0) || (PayloadBootInfo->MmNsCommBufSize == 0)) {
+    return EFI_INVALID_PARAMETER;
+  }
+
+  // Create MMBuffer status HOB. Valid bit set in this status buffer tells
+  // the MM that the buffer content is ready to be consumed.
+  MmCommBuffer = BuildGuidHob (&gMmCommBufferHobGuid, sizeof (MM_COMM_BUFFER));
+  if (MmCommBuffer == NULL) {
+    return EFI_OUT_OF_RESOURCES;
+  }
+
+  //
+  // Allocate runtime memory for MM communication status parameters :
+  // ReturnStatus, ReturnBufferSize, IsCommBufferValid
+  //
+  MmCommBuffer->PhysicalStart = PayloadBootInfo->MmNsCommBufBase + EFI_PAGE_SIZE;
+  MmCommBuffer->NumberOfPages = EFI_SIZE_TO_PAGES (PayloadBootInfo->MmNsCommBufSize - EFI_PAGE_SIZE);
+  MmCommBuffer->Status        = (EFI_PHYSICAL_ADDRESS)PayloadBootInfo->MmNsCommBufBase + sizeof (MM_COMM_BUFFER);
+  return EFI_SUCCESS;
+}
+
+/**
+  Use the boot information passed by privileged firmware to populate a HOB list
+  suitable for consumption by the MM Core and drivers.
+
+  @param  [in, out] CpuDriverEntryPoint   Address of MM CPU driver entrypoint
+  @param  [in]      PayloadBootInfo       Boot information passed by privileged
+                                          firmware
+
+**/
+VOID *
+CreateHobListFromBootInfo (
+  IN  OUT  PI_MM_CPU_DRIVER_ENTRYPOINT  *CpuDriverEntryPoint,
+  IN       EFI_RISCV_SMM_PAYLOAD_INFO   *PayloadBootInfo
+  )
+{
+  EFI_HOB_HANDOFF_INFO_TABLE      *HobStart;
+  EFI_RESOURCE_ATTRIBUTE_TYPE     Attributes;
+  UINT32                          Index;
+  UINT32                          BufferSize;
+  UINT32                          Flags;
+  EFI_MMRAM_HOB_DESCRIPTOR_BLOCK  *MmramRangesHob;
+  EFI_MMRAM_DESCRIPTOR            *MmramRanges;
+  MP_INFORMATION_HOB_DATA         *MpInformationHobData;
+  EFI_PROCESSOR_INFORMATION       *ProcInfoBuffer;
+  EFI_RISCV_SMM_CPU_INFO          *CpuInfo;
+  MM_CPU_DRIVER_EP_DESCRIPTOR     *CpuDriverEntryPointDesc;
+
+  // Create a hoblist with a PHIT and EOH
+  HobStart = HobConstructor (
+               (VOID *)(UINTN)PayloadBootInfo->MmMemBase,
+               (UINTN)PayloadBootInfo->MmMemLimit - PayloadBootInfo->MmMemBase,
+               (VOID *)(UINTN)PayloadBootInfo->MmHeapBase,
+               (VOID *)(UINTN)(PayloadBootInfo->MmHeapBase + PayloadBootInfo->MmHeapSize)
+               );
+
+  // Check that the Hoblist starts at the bottom of the Heap
+  ASSERT (HobStart == (VOID *)(UINTN)PayloadBootInfo->MmHeapBase);
+
+  // Build a Boot Firmware Volume HOB
+  BuildFvHob (PayloadBootInfo->MmImageBase, PayloadBootInfo->MmImageSize);
+
+  // Build a resource descriptor Hob that describes the available physical
+  // memory range
+  Attributes = (
+                EFI_RESOURCE_ATTRIBUTE_PRESENT |
+                EFI_RESOURCE_ATTRIBUTE_INITIALIZED |
+                EFI_RESOURCE_ATTRIBUTE_TESTED |
+                EFI_RESOURCE_ATTRIBUTE_UNCACHEABLE |
+                EFI_RESOURCE_ATTRIBUTE_WRITE_COMBINEABLE |
+                EFI_RESOURCE_ATTRIBUTE_WRITE_THROUGH_CACHEABLE |
+                EFI_RESOURCE_ATTRIBUTE_WRITE_BACK_CACHEABLE
+                );
+
+  // Find the size of the GUIDed HOB with MP information
+  BufferSize  = sizeof (MP_INFORMATION_HOB_DATA);
+  BufferSize += sizeof (EFI_PROCESSOR_INFORMATION) * PayloadBootInfo->NumCpus;
+
+  // Create a Guided MP information HOB to enable the CPU driver to
+  // perform per-cpu allocations.
+  MpInformationHobData = BuildGuidHob (&gMpInformationHobGuid, BufferSize);
+
+  // Populate the MP information HOB with the topology information passed by
+  // privileged firmware
+  MpInformationHobData->NumberOfProcessors        = PayloadBootInfo->NumCpus;
+  MpInformationHobData->NumberOfEnabledProcessors = PayloadBootInfo->NumCpus;
+  ProcInfoBuffer                                  = MpInformationHobData->ProcessorInfoBuffer;
+  CpuInfo                                         = &(PayloadBootInfo->CpuInfo);
+
+  for (Index = 0; Index < PayloadBootInfo->NumCpus; Index++) {
+    ProcInfoBuffer[Index].ProcessorId      = CpuInfo[Index].ProcessorId;
+    ProcInfoBuffer[Index].Location.Package = CpuInfo[Index].Package;
+    ProcInfoBuffer[Index].Location.Core    = CpuInfo[Index].Core;
+    ProcInfoBuffer[Index].Location.Thread  = 0; // not used
+
+    Flags = PROCESSOR_ENABLED_BIT | PROCESSOR_HEALTH_STATUS_BIT;
+    if (CpuInfo[Index].Flags & CPU_INFO_FLAG_PRIMARY_CPU) {
+      Flags |= PROCESSOR_AS_BSP_BIT;
+    }
+
+    ProcInfoBuffer[Index].StatusFlag = Flags;
+  }
+
+  if (MmBuildCommBufferHob (PayloadBootInfo) != EFI_SUCCESS) {
+    DEBUG ((DEBUG_ERROR, "Error building MmCommBufferHobs"));
+  }
+
+  // Create a Guided HOB to enable the CPU driver to share its entry
+  // point and populate it with the address of the shared buffer
+  CpuDriverEntryPointDesc = (MM_CPU_DRIVER_EP_DESCRIPTOR *)BuildGuidHob (
+                                                             &gEfiMmCpuDriverEpDescriptorGuid,
+                                                             sizeof (MM_CPU_DRIVER_EP_DESCRIPTOR)
+                                                             );
+
+  *CpuDriverEntryPoint                      = NULL;
+  CpuDriverEntryPointDesc->MmCpuDriverEpPtr = CpuDriverEntryPoint;
+
+  // Find the size of the GUIDed HOB with MMRAM ranges
+  BufferSize  = sizeof (EFI_MMRAM_HOB_DESCRIPTOR_BLOCK);
+  BufferSize += PayloadBootInfo->NumMmMemRegions * sizeof (EFI_MMRAM_DESCRIPTOR);
+
+  // Create a GUIDed HOB with MMRAM ranges
+  MmramRangesHob = BuildGuidHob (&gEfiMmPeiMmramMemoryReserveGuid, BufferSize);
+
+  // Fill up the number of MMRAM memory regions
+  MmramRangesHob->NumberOfMmReservedRegions = PayloadBootInfo->NumMmMemRegions;
+  // Fill up the MMRAM ranges
+  MmramRanges = &MmramRangesHob->Descriptor[0];
+
+  // Base and size of memory occupied by the Standalone MM image
+  MmramRanges[0].PhysicalStart = PayloadBootInfo->MmImageBase;
+  MmramRanges[0].CpuStart      = PayloadBootInfo->MmImageBase;
+  MmramRanges[0].PhysicalSize  = PayloadBootInfo->MmImageSize;
+  MmramRanges[0].RegionState   = EFI_CACHEABLE | EFI_ALLOCATED;
+
+  // Base and size of buffer shared with privileged Secure world software
+  MmramRanges[1].PhysicalStart = PayloadBootInfo->MmSharedBufBase;
+  MmramRanges[1].CpuStart      = PayloadBootInfo->MmSharedBufBase;
+  MmramRanges[1].PhysicalSize  = PayloadBootInfo->MmSharedBufSize;
+  MmramRanges[1].RegionState   = EFI_CACHEABLE | EFI_ALLOCATED;
+
+  // Base and size of buffer used for synchronous communication with Normal
+  // world software
+  MmramRanges[2].PhysicalStart = PayloadBootInfo->MmNsCommBufBase;
+  MmramRanges[2].CpuStart      = PayloadBootInfo->MmNsCommBufBase;
+  MmramRanges[2].PhysicalSize  = PayloadBootInfo->MmNsCommBufSize;
+  MmramRanges[2].RegionState   = EFI_CACHEABLE | EFI_ALLOCATED;
+
+  // Base and size of memory allocated for stacks for all cpus
+  MmramRanges[3].PhysicalStart = PayloadBootInfo->MmStackBase;
+  MmramRanges[3].CpuStart      = PayloadBootInfo->MmStackBase;
+  MmramRanges[3].PhysicalSize  = PayloadBootInfo->MmPcpuStackSize * PayloadBootInfo->NumCpus;
+  MmramRanges[3].RegionState   = EFI_CACHEABLE | EFI_ALLOCATED;
+
+  // Base and size of heap memory shared by all cpus
+  MmramRanges[4].PhysicalStart = (EFI_PHYSICAL_ADDRESS)(UINTN)HobStart;
+  MmramRanges[4].CpuStart      = (EFI_PHYSICAL_ADDRESS)(UINTN)HobStart;
+  MmramRanges[4].PhysicalSize  = HobStart->EfiFreeMemoryBottom - (EFI_PHYSICAL_ADDRESS)(UINTN)HobStart;
+  MmramRanges[4].RegionState   = EFI_CACHEABLE | EFI_ALLOCATED;
+
+  // Base and size of heap memory shared by all cpus
+  MmramRanges[5].PhysicalStart = HobStart->EfiFreeMemoryBottom;
+  MmramRanges[5].CpuStart      = HobStart->EfiFreeMemoryBottom;
+  MmramRanges[5].PhysicalSize  = HobStart->EfiFreeMemoryTop - HobStart->EfiFreeMemoryBottom;
+  MmramRanges[5].RegionState   = EFI_CACHEABLE;
+
+  return HobStart;
+}
--- MdePkg/Library/StandaloneMmCoreEntryPoint/RiscV64/StandaloneMmCoreEntryPoint.c
+++ MdePkg/Library/StandaloneMmCoreEntryPoint/RiscV64/StandaloneMmCoreEntryPoint.c
@@ -8,6 +8,55 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 
 #include <PiMm.h>
 
+#include <StandaloneMmCpu.h>
+#include <StandaloneMmCoreEntryPoint.h>
+
+PI_MM_CPU_DRIVER_ENTRYPOINT  CpuDriverEntryPoint = NULL;
+
+EFI_RISCV_SMM_PAYLOAD_INFO *
+GetAndPrintBootinformation (
+  IN VOID  *PayloadInfoAddress
+  )
+{
+  EFI_RISCV_SMM_PAYLOAD_INFO  *PayloadBootInfo;
+  EFI_RISCV_SMM_CPU_INFO      *PayloadCpuInfo;
+  UINTN                       Index;
+
+  PayloadBootInfo = (EFI_RISCV_SMM_PAYLOAD_INFO *)PayloadInfoAddress;
+
+  if (PayloadBootInfo == NULL) {
+    DEBUG ((DEBUG_ERROR, "PayloadBootInfo NULL\n"));
+    return NULL;
+  }
+
+  DEBUG ((DEBUG_INFO, "NumMmMemRegions - 0x%x\n", PayloadBootInfo->NumMmMemRegions));
+  DEBUG ((DEBUG_INFO, "MmMemBase       - 0x%lx\n", PayloadBootInfo->MmMemBase));
+  DEBUG ((DEBUG_INFO, "MmMemLimit      - 0x%lx\n", PayloadBootInfo->MmMemLimit));
+  DEBUG ((DEBUG_INFO, "MmImageBase     - 0x%lx\n", PayloadBootInfo->MmImageBase));
+  DEBUG ((DEBUG_INFO, "MmStackBase     - 0x%lx\n", PayloadBootInfo->MmStackBase));
+  DEBUG ((DEBUG_INFO, "MmHeapBase      - 0x%lx\n", PayloadBootInfo->MmHeapBase));
+  DEBUG ((DEBUG_INFO, "MmNsCommBufBase - 0x%lx\n", PayloadBootInfo->MmNsCommBufBase));
+  DEBUG ((DEBUG_INFO, "MmSharedBufBase - 0x%lx\n", PayloadBootInfo->MmSharedBufBase));
+
+  DEBUG ((DEBUG_INFO, "MmImageSize     - 0x%x\n", PayloadBootInfo->MmImageSize));
+  DEBUG ((DEBUG_INFO, "MmPcpuStackSize - 0x%x\n", PayloadBootInfo->MmPcpuStackSize));
+  DEBUG ((DEBUG_INFO, "MmHeapSize      - 0x%x\n", PayloadBootInfo->MmHeapSize));
+  DEBUG ((DEBUG_INFO, "MmNsCommBufSize - 0x%x\n", PayloadBootInfo->MmNsCommBufSize));
+  DEBUG ((DEBUG_INFO, "MmSharedBufSize - 0x%x\n", PayloadBootInfo->MmSharedBufSize));
+
+  DEBUG ((DEBUG_INFO, "NumCpus         - 0x%x\n", PayloadBootInfo->NumCpus));
+
+  PayloadCpuInfo = (EFI_RISCV_SMM_CPU_INFO *)&(PayloadBootInfo->CpuInfo);
+
+  for (Index = 0; Index < PayloadBootInfo->NumCpus; Index++) {
+    DEBUG ((DEBUG_INFO, "ProcessorId        - 0x%lx\n", PayloadCpuInfo[Index].ProcessorId));
+    DEBUG ((DEBUG_INFO, "Package            - 0x%x\n", PayloadCpuInfo[Index].Package));
+    DEBUG ((DEBUG_INFO, "Core               - 0x%x\n", PayloadCpuInfo[Index].Core));
+  }
+
+  return PayloadBootInfo;
+}
+
 /**
   The entry point of Standalone MM Foundation.
 
@@ -22,5 +71,25 @@ CModuleEntryPoint (
   IN VOID    *PayloadInfoAddress
   )
 {
+  EFI_RISCV_SMM_PAYLOAD_INFO  *PayloadBootInfo;
+  VOID                        *HobStart;
+
+  PayloadBootInfo = GetAndPrintBootinformation (PayloadInfoAddress);
+  if (PayloadBootInfo == NULL) {
+    return;
+  }
+
+  //
+  // Create Hoblist based upon boot information passed by privileged software
+  //
+  HobStart = CreateHobListFromBootInfo (&CpuDriverEntryPoint, PayloadBootInfo);
+
+  //
+  // Call the MM Core entry point
+  //
+  ProcessModuleEntryPointList (HobStart);
+
+  DEBUG ((DEBUG_INFO, "Cpu Driver EP %p\n", (VOID *)CpuDriverEntryPoint));
+
   ASSERT (0);
 }
